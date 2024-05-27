8985552413370263a4abd6dc2dc494e0f148fffe
Nirav Joshi <niravjoshi@Niravs-MacBook-Pro.local>
2024-05-27 02:03:57 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'slot-machine',
+  webDir: 'www'
+};
+
+export default config;
